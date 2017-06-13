ce5af14dc8f36db74bb1498933b80c9901ebacc0
François mathieu <maps_red@yahoo.fr>
2017-06-13 22:51:29 +0200

    Create François.php

---
+++ François.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maps_red
+ * Date: 13/06/2017
+ * Time: 22:08
+ */
+
+$referential = [
+    ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+    ["M", "MM", "MMM"],
+];
+
+//$number = (string)$_POST['nombre'];
+$number = (string)50;
+$taille = strlen($number);
+$final = [];
+
+for ($i = $taille - 1; $i >= 0; $i--) {
+    if (-1 < $key = $number[$i] - 1) {
+        $final[] = $referential[$i][$key];
+    }
+}
+
+
+//var_dump($final);
+var_dump(implode("", $final));
+
