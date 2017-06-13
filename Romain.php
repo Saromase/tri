81564ab19bc9d95467ef015f8c537e338a45ba02
Saromase <seite.romain@hotmail.fr>
2017-06-13 21:16:18 +0200

    opti test Convertisseur

--- Romain.php
+++ Romain.php
@@ -3,13 +3,14 @@
 <form action=" " method="post">
     <h2>Entrez votre nombre</h2>
     <p>Centaines</p>
-    <input type="text" name="centaine">
+    <input type="number" name="centaine">
     <p> Dixaines</p>
-    <input type="text" name="dixaine">
+    <input type="number" name="dixaine">
     <p>Unites</p>
-    <input type="text" name="unite">
+    <input type="number" name="unite">
     <input type="submit" value="OK">
 </form>
+
 </body>
 </html>
 
@@ -20,28 +21,17 @@
  * Date: 12/06/2017
  * Time: 22:05
  */
-$arrayC = [' ','C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM' ];
+
+$arrayC = [' ', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
 $arrayD = [' ', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
 $arrayU = [' ', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-
-
-$postC = $_POST['centaine'];
-$postD = $_POST['dixaine'];
-$postU = $_POST['unite'];
-
-
-$nbC = $arrayC[$postC];
-$nbD = $arrayD[$postD];
-$nbU = $arrayU[$postU];
-
-if (( $postC or $postD or $postU) != NULL) {
-    echo 'vous avez saisie le nombre ', $nbC, $nbD, $nbU;
+if (($_POST['centaine'] or $_POST['dixaine'] or $_POST['unite']) != NULL) {
+    echo 'vous avez saisie le nombre ', $arrayC[$_POST['centaine']], $arrayD[$_POST['dixaine']], $arrayU[$_POST['unite']];
 } else {
-    echo " Veuillez saisir un nombre svp";
+    echo 'Veuillez saisir un nombre svp';
 }
 
-$toto = (string)$postU;
-var_dump($toto);
-
 ?>
+
+
 
